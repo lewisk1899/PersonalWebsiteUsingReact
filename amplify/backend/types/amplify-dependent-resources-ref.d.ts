6543780bc81251981beff5815c18887e275db770
User <user@dhcp-10-134-146-65.uawifi.arizona.edu>
2022-10-15 23:07:15 -0700

    AWS Amplify now working and site is running

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "mlapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    },
+    "function": {
+        "personalwebsiteusing5f42940a": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
